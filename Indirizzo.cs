b502b2c4605b3d5ae1b2523f9577c953e7242ea0
Levva01 <vellanifederico13@gmail.com>
2022-09-23 16:14:20 +0200

    stampa a schermo della lista completata

---
+++ Indirizzo.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Indirizzo
+{
+    public string Name { get; }
+    public string Surname { get; }
+    public string Street { get; }
+    public string City { get; }
+    public string Province { get; }
+    public string Zip { get; }
+
+    public Indirizzo(string Name, string Surname, string Street, string City, string Province, string Zip)
+    {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Street = Street;
+        this.City = City;
+        this.Province = Province;
+        this.Zip = Zip;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿StreamReader fileIndirizzi = File.OpenText("C:\\Users\\vella\\source\\repos\\csharp-lista-indirizzi\\Indirizzi-first.txt");
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+fileIndirizzi.ReadLine();
+
+while (!fileIndirizzi.EndOfStream)
+{
+    string riga = fileIndirizzi.ReadLine();
+
+    string[] split = riga.Split(',');
+
+    try
+    {
+        string name = split[0];
+        string surname = split[1];
+        string street = split[2];
+        string city = split[3];
+        string province = split[4];
+        string zip = split[5];
+        name = name.Replace("- ", "");
+        Indirizzo indirizzo = new Indirizzo(name, surname, street, city, province, zip);
+        listaIndirizzi.Add(indirizzo);
+        Console.WriteLine("{0} importato correttamente", street);
+
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("Indirizzo non valido -->" + riga);
+    }
+
+
+}
+
+fileIndirizzi.Close();
+
+try
+{
+    StreamWriter indirizziWriter = File.CreateText("Indirizzi.txt");
+
+    indirizziWriter.WriteLine("Libri preferiti formato bello");
+
+    foreach (Indirizzo indirizzo in listaIndirizzi)
+    {
+        Console.WriteLine("Salvataggio {0}", indirizzo.Name);
+        indirizziWriter.WriteLine();
+        indirizziWriter.WriteLine(indirizzo.ToString());
+    }
+
+    indirizziWriter.Close();
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Qualcosa è andato storto: {0}", e.Message);
+}
